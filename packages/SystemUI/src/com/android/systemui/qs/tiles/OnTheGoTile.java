BenzoRom/frameworks_base

QS: SystemUI: Add On-The-Go Tile
Change-Id: Ic345f83723a48e3a7d610926fb9b47fab7ef878f
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2018 Benzo Rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+
+import com.android.internal.util.benzo.OnTheGoActions;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+/** Quick settings tile: OnTheGo Mode **/
+public class OnTheGoTile extends QSTileImpl<BooleanState> {
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_onthego);
+
+    public OnTheGoTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+
+    protected void toggleState() {
+        Intent service = (new Intent())
+                .setClassName("com.android.systemui",
+                "com.android.systemui.benzo.onthego.OnTheGoService");
+        OnTheGoActions.processAction(mContext,
+                OnTheGoActions.ACTION_ONTHEGO_TOGGLE);
+    }
+
+    @Override
+    protected void handleClick() {
+        refreshState();
+        toggleState();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_onthego_label);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.contentDescription =  mContext.getString(
+                R.string.quick_settings_onthego_label);
+        state.label = mContext.getString(R.string.quick_settings_onthego_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_onthego);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.BENZO;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        return mContext.getString(R.string.quick_settings_onthego_label);
+    }
+}

KCUFRom/android_framework_base

packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2018 The KCUF Rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+
+import com.android.internal.util.kcuf.OnTheGoActions;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+/** Quick settings tile: OnTheGo Mode **/
+public class OnTheGoTile extends QSTileImpl<BooleanState> {
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_onthego);
+
+    public OnTheGoTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+
+    protected void toggleState() {
+        Intent service = (new Intent())
+                .setClassName("com.android.systemui",
+                "com.android.systemui.kcuf.onthego.OnTheGoService");
+        OnTheGoActions.processAction(mContext,
+                OnTheGoActions.ACTION_ONTHEGO_TOGGLE);
+    }
+
+    @Override
+    protected void handleClick() {
+        refreshState();
+        toggleState();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_onthego_label);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.contentDescription =  mContext.getString(
+                R.string.quick_settings_onthego_label);
+        state.label = mContext.getString(R.string.quick_settings_onthego_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_onthego);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.KCUF_SETTINGS;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        return mContext.getString(R.string.quick_settings_onthego_label);
+    }
+}